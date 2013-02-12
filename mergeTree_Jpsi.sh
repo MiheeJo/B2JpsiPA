6c098dc37e9a250247d5eb22c0f4684c5a742efb
miheejo <>
2013-02-12 13:56:12 +0000

    More options added for runType.

--- mergeTree_Jpsi.sh
+++ mergeTree_Jpsi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 eval `scramv1 runtime -sh`
 
-prefixarr=(default_bit1_singleMuEtaAll default_bit1_mu4GeV default_bit1_singleMuEtaBarrel)
+prefixarr=(default_bit1_singleMuEtaAll default_bit1_singleMuEtaAllMu4GeV default_bit1_mu4GeV default_bit1_singleMuEtaBarrel)
 labelarr=(305070 50 60 70 80 805 901235)
 
 for prefix in ${prefixarr[@]}; do
--- runTree_dphi.sh
+++ runTree_dphi.sh
@@ -4,10 +4,10 @@ eval `scramv1 runtime -sh`
 #inputf=root://eoscms//eos/cms
 #inputf=$inputf/store/group/phys_heavyions/dileptons/Data2013/pPb/Prompt/TTrees/Jpsi_Histos_Runs_210498-210837.root
 
-inputf=/afs/cern.ch/work/m/miheejo/private/TREE/pA/Mass2-5GeV_Jpsi_Histos_Runs_210498-211256.root
-#inputf=/afs/cern.ch/work/m/miheejo/private/TREE/pA/Mass2-5GeV_Jpsi_Histos_Runs_210498-210909.root
+inputf=/afs/cern.ch/work/m/miheejo/private/TREE/pA/Mass2-5GeV_Jpsi_Histos_Runs_211371-211460.root
+#inputf=/afs/cern.ch/work/m/miheejo/private/TREE/pA/Mass2-5GeV_Jpsi_Histos_Runs_210498-211256.root
 
-prefixarr=(default_bit1_singleMuEtaAll default_bit1_mu4GeV default_bit1_singleMuEtaBarrel)
+prefixarr=(default_bit1_mu4GeV default_bit1_singleMuEtaAll default_bit1_singleMuEtaAllMu4GeV default_bit1_singleMuEtaBarrel)
 
 labelarr=(305070 50 60 70 80 805 901235)
 
@@ -24,16 +24,21 @@ for prefix in ${prefixarr[@]}; do
   fi
 
   if [ "$prefix" == "${prefixarr[0]}" ]; then
-    echo "runType == 8"
-    for label in ${labelarr[@]}; do
-      ./Tree2Datasets =t $label =or 8 =f $inputf $prefix/cent$label >& $prefix/log_cent$label &
-    done
-  elif [ "$prefix" == "${prefixarr[1]}" ]; then
     echo "runType == 7"
     for label in ${labelarr[@]}; do
       ./Tree2Datasets =t $label =or 7 =f $inputf $prefix/cent$label >& $prefix/log_cent$label &
     done
+  elif [ "$prefix" == "${prefixarr[1]}" ]; then
+    echo "runType == 8"
+    for label in ${labelarr[@]}; do
+      ./Tree2Datasets =t $label =or 8 =f $inputf $prefix/cent$label >& $prefix/log_cent$label &
+    done
   elif [ "$prefix" == "${prefixarr[2]}" ]; then
+    echo "runType == 78"
+    for label in ${labelarr[@]}; do
+      ./Tree2Datasets =t $label =or 78 =f $inputf $prefix/cent$label >& $prefix/log_cent$label &
+    done
+  elif [ "$prefix" == "${prefixarr[3]}" ]; then
     echo "runType == 9"
     for label in ${labelarr[@]}; do
       ./Tree2Datasets =t $label =or 9 =f $inputf $prefix/cent$label >& $prefix/log_cent$label &
