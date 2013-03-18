e581cb8e53a2ffbc279f4911e6bb642f9fd2d560
miheejo <>
2013-03-18 16:02:16 +0000

    minor changes

--- pPb_nRaps4.sh
+++ pPb_nRaps4.sh
@@ -11,11 +11,12 @@ prefix=$3
 ################################################################ 
 ########## Script parameter setting
 ################################################################ 
-# Analysis home directory
-home=/afs/cern.ch/work/m/miheejo/private/cms442_Jpsi/src/pA/JpsiRpA/
 # Storage for batch jobs (should be under /afs)
 storage=$(pwd)/Results/$prefix
 scripts=$(pwd)/Scripts
+if [ ! -d $(pwd)/Results ]; then
+  mkdir $(pwd)/Results
+fi
 if [ ! -d $storage ]; then
   mkdir $storage
 fi
@@ -30,9 +31,12 @@ is2Widths=1
 isPEE=1
 
 # Non-prompt MC
-mc1=/afs/cern.ch/work/m/miheejo/private/cms442_Jpsi/src/pA/datasets_mc_538hi/pPb/nonPrompt/Data2013_cent0-100_dPhi0.000-1.571.root
+mc1=/afs/cern.ch/work/m/miheejo/private/cms442_Jpsi/src/pA/datasets_mc_538hi/pPb/nonPrompt_singleMuEtaAll_bit1/Data2013_cent0-100_dPhi0.000-1.571.root
 # Prompt MC
-mc2=/afs/cern.ch/work/m/miheejo/private/cms442_Jpsi/src/pA/datasets_mc_538hi/pPb/prompt/Data2013_cent0-100_dPhi0.000-1.571.root
+mc2=/afs/cern.ch/work/m/miheejo/private/cms442_Jpsi/src/pA/datasets_mc_538hi/pPb/prompt_singleMuEtaAll_bit1/Data2013_cent0-100_dPhi0.000-1.571.root
+
+# Choose ctau error file
+ctauErrFile=/afs/cern.ch/work/m/miheejo/private/cms442_Jpsi/src/pA/ctauErrCheck/pPb12.55nb/ctauErrRange_singleMuEtaAll_bit1.txt;
 
 mSigF="sigCB2WNG1" # Mass signal function name (options: sigCB2WNG1 (default), signalCB3WN)
 mBkgF="expFunct" # Mass background function name (options: expFunct (default), polFunct)
@@ -46,45 +50,20 @@ anaBct=1 #0: do b-fit(not-analytic fit for b-lifetime), 1: do b-fit(analytic fit
 resOpt=2
 ctauBkg=0 #0: 1 ctau bkg, 1: 2 ctau bkg with signal region fitting, 2: 2 ctau bkg with step function
 
-rapbins=(-2.4--1.47 -1.47--0.53 -0.53-0.47 0.47-1.47 1.47-2.4 -1.47-0.47 0.47-2.4 -0.53-1.47)
-dphibins=(0.000-1.571) #0.000-0.393 0.393-0.785 0.785-1.178 1.178-1.571)
-cent0024=(0-5 5-10 10-15 15-20 20-25 25-30 30-35 35-40 40-45 45-50 50-55 55-60 60-65 65-70) #0-1 1-2 2-4 4-6 6-8 8-10 10-12 12-15 15-18 18-21 21-24 24-27 27-30 30-33 33-36 36-39 39-42 42-45 45-48 48-51 51-54 54-57 57-60 60-65 65-70 70-75 75-80 80-90 90-100)
-centcorser=(0-10 10-20 20-30 30-40 40-50 50-60 60-70 70-80 70-100 60-100 50-100 50-70 80-100 10-30 30-50)
+rapbins=(-1.47--0.53 -0.53-0.47 0.47-1.47 1.47-2.4 -1.47-0.47 0.47-2.4 -0.53-1.47)
+#centfiner=(0-5 5-10 10-15 15-20 20-25 25-30 30-35 35-40 40-45 45-50 50-55 55-60 60-65 65-70) #0-1 1-2 2-4 4-6 6-8 8-10 10-12 12-15 15-18 18-21 21-24 24-27 27-30 30-33 33-36 36-39 39-42 42-45 45-48 48-51 51-54 54-57 57-60 60-65 65-70 70-75 75-80 80-90 90-100)
+centcorser=(0-10 10-20 20-30 30-40 40-50 50-60 60-70 70-100 60-100 50-100 50-70 10-30 30-50)
 centcorser2=(0-10 10-30 30-50 50-100)
-#pt0024=(6.5-30.0 6.5-7.5 7.5-8.5 8.5-9.5 9.5-10.5 10.5-11.5 11.5-13.0 13.0-18.0 18.0-30.0)
-pt0024=(0.0-3.0 3.0-6.5)
-ptcorser=(0.0-6.5 6.5-10.0 10.0-30.0 6.5-8.0 8.0-10.0 10.0-13.0 13.0-30.0)
-ptcorser2=(0.0-6.5 6.5-10.0 10.0-30.0)
+ptlower=(0.0-3.0 3.0-6.5 0.0-6.5)
+ptlower2=(3.0-6.5)
+ptcorser=(6.5-10.0 10.0-30.0 6.5-8.0 8.0-10.0 10.0-13.0 13.0-30.0)
+ptcorser2=(6.5-10.0 10.0-30.0)
+dphibins=(0.000-1.571) #0.000-0.393 0.393-0.785 0.785-1.178 1.178-1.571)
 
 
 ################################################################ 
 ########## Information print out
 ################################################################ 
-# Choose ctau error file
-if [ "$ispbpb" == "1" ]; then
-  ctauErrF=$(echo $prefix | awk 'BEGIN{FS="_"}; {print $2}')
-  if [ "$ctauErrF" == "bit1" -o "$ctauErrF" == "cowboy" -o "$ctauErrF" == "sailor" ]; then
-    ctauErrFile=/afs/cern.ch/work/m/miheejo/private/cms442_Jpsi/src/ctauErrCheck/ctauErrRange_default_$ctauErrF\_oldctau.txt;
-  else
-    ctauErrFile=/afs/cern.ch/work/m/miheejo/private/cms442_Jpsi/src/ctauErrCheck/ctauErrRange_default_bit1_oldctau.txt;
-  fi
-elif [ "$ispbpb" == "2" ]; then
-  ctauErrF=$(echo $prefix | awk 'BEGIN{FS="_"}; {print $2}')
-  if [ "$ctauErrF" == "singleMuEtaAll" -o "$ctauErrF" == "singleMuEtaAllMu4GeV" -o "$ctauErrF" == "singleMuEtaBarrel" -o "$ctauErrF" == "mu4GeV" ]; then
-    ctauErrFile=/afs/cern.ch/work/m/miheejo/private/cms442_Jpsi/src/pA/ctauErrCheck/pPb12.55nb/ctauErrRange_$ctauErrF\_bit1.txt;
-  else
-    ctauErrFile=/afs/cern.ch/work/m/miheejo/private/cms442_Jpsi/src/pA/ctauErrCheck/pPb12.55nb/ctauErrRange_default_bit1.txt;
-  fi
-else
-  ctauErrF=$(echo $prefix | awk 'BEGIN{FS="_"}; {print $2}')
-  if [ "$ctauErrF" == "bit1" -o "$ctauErrF" == "cowboy" -o "$ctauErrF" == "sailor" ]; then
-    ctauErrFile=/afs/cern.ch/work/m/miheejo/private/cms442_Jpsi/src/ctauErrCheck_pp/ctauErrRange_default_$ctauErrF\_oldctau.txt;
-  else
-    ctauErrFile=/afs/cern.ch/work/m/miheejo/private/cms442_Jpsi/src/ctauErrCheck_pp/ctauErrRange_default_bit1_oldctau.txt;
-  fi
-fi
-echo "#### ctauErrFile: " $ctauErrFile
-
 txtrst=$(tput sgr0)
 txtred=$(tput setaf 2)  #1 Red, 2 Green, 3 Yellow, 4 Blue, 5 Purple, 6 Cyan, 7 White
 txtbld=$(tput bold)     #dim (Half-bright mode), bold (Bold characters)
@@ -119,17 +98,17 @@ function program {
       printf "cd %s\n" $(pwd) >> $scripts/$work.csh
       printf "eval \`scramv1 runtime -csh\`\n" >> $scripts/$work.csh
       printf "cd -\n" >> $scripts/$work.csh
-      printf "cp %s/%s %s/fit2Data.h %s/fit2DData_pbp.cpp .\n" $(pwd) Scripts/$work.csh $(pwd) $(pwd) >> $scripts/$work.csh
+      printf "cp %s/%s %s/fit2DData.h %s/fit2DData_ppb.cpp .\n" $(pwd) Scripts/$work.csh $(pwd) $(pwd) >> $scripts/$work.csh
 
       ctauerrMB=$(awk -v rap=$rap -v pt=$pt  '{if ($1==rap && $2==pt && $3=="0-100" && $4=="0.000-1.571") {print $5}  }' < $ctauErrFile)
       ctauerrPhi=$(awk -v rap=$rap -v pt=$pt -v cent=$cent -v dphi=0.000-1.571 '{if ($1==rap && $2==pt && $3==cent && $4==dphi) {print $5}  }' < $ctauErrFile)
       ctauerr=$(awk -v rap=$rap -v pt=$pt -v cent=$cent -v dphi=$dphi '{if ($1==rap && $2==pt && $3==cent && $4==dphi) {print $5}  }' < $ctauErrFile)
 
       if [ "$ctauerrMB" == "" -o "$ctauerrPhi" == "" -o "$ctauerr" == "" ]; then
-        echo "This bin doesn't have pre-defined ctau error range. Set ctau error range to 0.01-0.16"
-        ctauerrMB=0.01-0.16
-        ctauerrPhi=0.01-0.16
-        ctauerr=0.01-0.16
+        echo "This bin doesn't have pre-defined ctau error range. Set ctau error range to 0.01-0.2"
+        ctauerrMB=0.01-0.2
+        ctauerrPhi=0.01-0.2
+        ctauerr=0.01-0.2
       fi
 
       if [ "$cent" == "0-100" ]; then
@@ -158,7 +137,7 @@ function program {
         fi
       fi
 
-      printf "tar zcvf %s.tgz %s* fit2DData.h fit2DData_pbp.cpp\n" $work $work >> $scripts/$work.csh
+      printf "tar zcvf %s.tgz %s* fit2DData.h fit2DData_ppb.cpp\n" $work $work >> $scripts/$work.csh
       printf "cp %s.tgz %s\n" $work $storage >> $scripts/$work.csh
       bsub -R "pool>10000" -u mihee.jo@cer.c -q 1nd -J $work < $scripts/$work.csh
     done
@@ -169,18 +148,42 @@ function program {
 ########## Running script with pre-defined binnings
 ################################################################ 
 for rap in ${rapbins[@]}; do
-  # pt dependence
-  for pt in ${pt0024[@]}; do
-    program $rap $pt 0-100
-    program $rap $pt ${centcorser2[@]}
-  done
   program $rap 6.5-30.0 0-100
+  
+  # pt dependence
+  case $rap in
+    "-0.47-0.53"|"-0.53-0.47")
+    for pt in ${ptcorser[@]}; do
+      program $rap $pt 0-100
+      program $rap $pt ${centcorser[@]}
+    done
+    ;;
+    "-1.47--0.53"|"0.47-1.47"|"-0.53-1.47"|"-1.47--0.47"|"0.53-1.47"|"-1.47-0.53")
+    for pt in ${ptcorser[@]}; do
+      program $rap $pt 0-100
+      program $rap $pt ${centcorser[@]}
+    done
+    for pt in ${ptlower2[@]}; do
+      program $rap $pt 0-100
+      program $rap $pt ${centcorser[@]}
+    done
+    ;;
+    *)
+    for pt in ${ptcorser[@]}; do
+      program $rap $pt 0-100
+      program $rap $pt ${centcorser[@]}
+    done
+    for pt in ${ptlower[@]}; do
+      program $rap $pt 0-100
+      program $rap $pt ${centcorser[@]}
+    done
+    ;;
+  esac
+  
   # fine cent dependence
-#    program $rap 6.5-30.0 ${cent0024[@]}
+#    program $rap 6.5-30.0 ${centfiner[@]}
+
+  # cent dependence
   program $rap 6.5-30.0 ${centcorser[@]}
-  # corser pt & corser cent dependence
-  for pt in ${ptcorser[@]}; do
-    program $rap $pt ${centcorser[@]}
-    program $rap $pt 0-100
-  done
+  
 done
