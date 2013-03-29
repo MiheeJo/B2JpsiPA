29496c5ae54ac3df3dd9de11fa7a64ceae8b126b
miheejo <>
2013-03-29 06:33:03 +0000

    change prefix array

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 #./Pbp_nRaps4.sh Fit2DDataPbp datasets/Pbp18.38nb/default_bit1                 Pbp_default_bit1 >& log_Pbp_default_bit1 &
-#./Pbp_mu4GeV.sh Fit2DDataPbp datasets/Pbp18.38nb/default_bit1_mu4GeV          Pbp_default_mu4GeV >& log_Pbp_default_mu4GeV &
-./Pbp_nRaps4_etaAll.sh Fit2DDataPbp datasets/Pbp18.38nb/default_bit1_singleMuEtaAll  Pbp_default_singleMuEtaAll >& log_Pbp_default_singleMuEtaAll &
+./Pbp_nRaps4_etaAll.sh Fit2DDataPbp datasets/Pbp18.38nb/singleMuEtaAll_bit1 Pbp_singleMuEtaAll_bit1 >& log_Pbp_singleMuEtaAll_bit1 &
 
 #./pPb_nRaps4.sh Fit2DDatapPb datasets/pPb12.55nb/default_bit1                 pPb_default_bit1 >& log_pPb_default_bit1 &
-#./pPb_mu4GeV.sh Fit2DDatapPb datasets/pPb12.55nb/default_bit1_mu4GeV          pPb_default_mu4GeV >& log_pPb_default_mu4GeV &
-./pPb_nRaps4_etaAll.sh Fit2DDatapPb datasets/pPb12.55nb/default_bit1_singleMuEtaAll  pPb_default_singleMuEtaAll >& log_pPb_default_singleMuEtaAll &
+./pPb_nRaps4_etaAll.sh Fit2DDatapPb datasets/pPb12.55nb/singleMuEtaAll_bit1 pPb_singleMuEtaAll_bit1 >& log_pPb_singleMuEtaAll_bit1 &
 
 
